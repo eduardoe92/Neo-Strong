a632923b300df39793b42bdd203d5db694c88959
eduardoe92 <eduardo.elias.1192@gmail.com>
2026-05-20 23:16:39 -0300

    App

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout/Layout';
+import Home from './pages/Home';
+import ItemListContainer from './pages/ItemListContainer';
+import ItemDetail from './pages/ItemDetail';
+import Contacto from './pages/Contacto';
+import Login from './pages/Login';
+import Carrito from './pages/Carrito';
+import Registro from './pages/Registro'; 
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="productos" element={<ItemListContainer />} />
+          <Route path="productos/:categoria" element={<ItemListContainer />} />
+          <Route path="item/:id" element={<ItemDetail />} />
+          <Route path="contacto" element={<Contacto />} />
+          <Route path="login" element={<Login />} />
+          <Route path="registro" element={<Registro />} />
+          <Route path="carrito" element={<Carrito />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
